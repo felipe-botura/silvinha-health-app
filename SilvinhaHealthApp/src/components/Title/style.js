4676d2053f1afcac2743fe7cb17357d01d7f246f
felipe-botura <felipebotura7@gmail.com>
2024-03-04 12:23:07 -0300

    Terminado o componente Title e começando o componente Form

--- SilvinhaHealthApp/src/components/Title/style.js
+++ SilvinhaHealthApp/src/components/Title/style.js
@@ -1,15 +1,15 @@
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#e0e5e5',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      color: '#ff0043',
-      fontSize: 28,
-      fontWeight: 'bold'
-    }
-  });
+  boxTitle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  textTitle: {
+    color: '#ff0043',
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+});
