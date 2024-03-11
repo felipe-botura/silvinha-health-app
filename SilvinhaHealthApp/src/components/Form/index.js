6a28ea322fa1c01d87584d8b85edce4dce7c7ab2
felipe-botura <felipebotura7@gmail.com>
2024-03-11 12:19:10 -0300

    Finalização e estilização do Form e Resultimc

--- SilvinhaHealthApp/src/components/Form/index.js
+++ SilvinhaHealthApp/src/components/Form/index.js
@@ -1,20 +1,33 @@
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import styles from './style'
+import ResultImc from './Resultimc'
 
 export default function Form() {
   return (
     <View style={styles.formContext}>
       <View style={styles.formGroup}>
-        <Text>Altura</Text>
-        <TextInput />
-        <Text>Peso</Text>
-        <TextInput />
-        <TouchableOpacity onPress={() => alert("O CABO C TÁ AI?")}>
-          <Text>Calcular IMC</Text>
+        <Text style={styles.formLabel}>Altura</Text>
+        <TextInput 
+          style={styles.input}
+          placeholder='Ex. 1.70'
+          keyboardType='numeric'
+        />
+        <Text style={styles.formLabel}>Peso</Text>
+        <TextInput 
+          style={styles.input}
+          placeholder='Ex. 80.5'
+          keyboardType='numeric'
+        />
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={() => alert("O CABO C TÁ AI?")}>
+          <Text style={styles.textButton}>
+            Calcular IMC
+          </Text>
         </TouchableOpacity>
-
       </View>
+      <ResultImc />
     </View>
   )
 }
