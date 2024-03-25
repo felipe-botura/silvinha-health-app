74e38e7741ee55f876c87d6beaf9e701be1d9444
felipe-botura <felipebotura7@gmail.com>
2024-03-25 12:24:49 -0300

    Finalizado

--- SilvinhaHealthApp/src/components/Form/index.js
+++ SilvinhaHealthApp/src/components/Form/index.js
@@ -1,33 +1,63 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native'
+import React, { useState } from 'react'
 import styles from './style'
-import ResultImc from './Resultimc'
+import ResultImc from './ResultImc'
 
 export default function Form() {
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [imc, setImc] = useState(null);
+  const [messageImc, setMessageImc] = useState("Preencha o peso e a altura");
+  const [textButton, setTextButton] = useState("Calcular IMC");
+
+  function imcCalculator () {
+    return setImc((weight / (height * height)).toFixed(2));
+  }
+
+  function validationImc () {
+    if (weight != null && height != null){
+      imcCalculator();
+      Keyboard.dismiss();
+      setHeight(null);
+      setWeight(null);
+      setMessageImc("Seu IMC é:")
+      setTextButton("Calcular Novamente")
+      return;
+    }    
+    setImc(null);
+    setMessageImc("Preencha o peso e a altura")
+    setTextButton("Calcular IMC")
+  }
+
   return (
     <View style={styles.formContext}>
       <View style={styles.formGroup}>
         <Text style={styles.formLabel}>Altura</Text>
         <TextInput 
           style={styles.input}
+          onChangeText={setHeight}
+          value={height ?? ""}
           placeholder='Ex. 1.70'
           keyboardType='numeric'
         />
         <Text style={styles.formLabel}>Peso</Text>
         <TextInput 
+          onChangeText={setWeight}
+          value={weight ?? ""}
           style={styles.input}
           placeholder='Ex. 80.5'
           keyboardType='numeric'
         />
         <TouchableOpacity 
           style={styles.button}
-          onPress={() => alert("O CABO C TÁ AI?")}>
+          onPress={() => validationImc()}
+        >
           <Text style={styles.textButton}>
-            Calcular IMC
+            {textButton}
           </Text>
         </TouchableOpacity>
       </View>
-      <ResultImc />
+      <ResultImc messageResult ={messageImc} resultIMC={imc} />
     </View>
   )
 }
